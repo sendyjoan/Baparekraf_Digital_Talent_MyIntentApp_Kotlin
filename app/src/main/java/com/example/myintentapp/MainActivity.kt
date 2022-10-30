4dc00685647f1039828ea05eccbde101a0f2721b
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-10-31 00:15:45 +0700

    Update MainActivity.kt
    Add Intent

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myintentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
             }
         }
     }
